1d909cf065d81c5a750e2a6997d9075c49e4fdd3
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-14 11:01:17 +0700

    fix:[major] setup prisma and many more

---
+++ src/server/trpc.ts
@@ -0,0 +1,18 @@
+import { initTRPC } from "@trpc/server";
+import { Context } from "./context";
+import superjson from "superjson";
+
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
+
+export const router = t.router;
+
+export const publicProcedure = t.procedure;
+
+export const middleware = t.middleware;
+
+export const mergeRouters = t.mergeRouters;
