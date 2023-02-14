d18f8b37dd321c0ac891a57b7e3e0bf36143569d
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-14 14:50:35 +0700

    fix:[major] update database and shit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import "~/styles/globals.css";
-import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+import type { AppType, AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import { DefaultLayout } from "~/components/DefaultLayout";
+
+import { trpc } from "~/utils/trpc";
+
+export type NextPageWithLayout<
+  TProps = Record<string, unknown>,
+  TInitialProps = TProps
+> = NextPage<TProps, TInitialProps> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const MyApp = (({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout =
+    Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
+
+  return getLayout(<Component {...pageProps} />);
+}) as AppType;
+
+export default trpc.withTRPC(MyApp);
