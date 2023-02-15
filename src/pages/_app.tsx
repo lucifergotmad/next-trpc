6ee0fa73fe41dd24ce9c88b68d0b5035456aab00
lucifergotmad <amateur.fullstackdev@gmail.com>
2023-02-15 22:54:46 +0700

    fix:[major] update the structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import type { AppType, AppProps } from "next/app";
 import type { ReactElement, ReactNode } from "react";
 
-import { DefaultLayout } from "~/components/DefaultLayout";
+import { DefaultLayout } from "~/shared/components/DefaultLayout";
 
 import { trpc } from "~/utils/trpc";
 
